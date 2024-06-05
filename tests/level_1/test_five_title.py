0260a859053dca95884b69a15cdfa6b8f69eda0e
Mikhail Artemov <babanlive@gmail.com>
2024-06-05 12:50:00 +0700

    5 done

--- tests/level_1/test_five_title.py
+++ tests/level_1/test_five_title.py
@@ -1,5 +1,16 @@
+import pytest
+
 from functions.level_1.five_title import change_copy_item
 
-
-def test_change_copy_item():
-    pass
+@pytest.mark.parametrize(
+    "title, max_main_item_title_length, expected",
+    [
+        ('Title', 100, 'Copy of Title'),
+        ('Title', 10, 'Title'),
+        ('Copy of Title', 100, 'Copy of Title (2)'),
+        ('Copy of Title (2)', 100, 'Copy of Title (3)'),
+        ('Copy of Title(1)' , 100, 'Copy of (2)'),
+    ]
+)
+def test_change_copy_item(title, max_main_item_title_length, expected):
+    assert change_copy_item(title, max_main_item_title_length) == expected
