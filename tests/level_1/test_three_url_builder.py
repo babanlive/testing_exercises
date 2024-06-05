c3b919c9bc0563d8cabb5c082c661e8c9d437855
Mikhail Artemov <babanlive@gmail.com>
2024-06-05 09:01:42 +0700

    three done

--- tests/level_1/test_three_url_builder.py
+++ tests/level_1/test_three_url_builder.py
@@ -2,4 +2,7 @@ from functions.level_1.three_url_builder import build_url
 
 
 def test_build_url():
-    pass
+    assert build_url("example.com", "relative_url") == "example.com/relative_url"
+    assert build_url("example.com", "relative_url", {"key": "value"}) == "example.com/relative_url?key=value"
+    assert build_url("example.com", "relative_url", {"key1": "value1", "key2": "value2"}) == "example.com/relative_url?key1=value1&key2=value2"
+    assert build_url("example.com", "relative_url", None) == "example.com/relative_url"
